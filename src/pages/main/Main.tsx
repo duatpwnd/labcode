3e43c848338f1edbacc313b399a1513b78301dd1
duatpwnd <duatpwnd@snaptag.co.kr>
2021-12-10 14:36:09 +0900

    add typing animation

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,12 +1,27 @@
 import { useEffect, useRef, useState } from "react"
+import Typing from 'react-typing-animation';
 import "./Main.scoped.scss"
+const AnimatedTypingComponent = () => {
+    return (
+        <Typing hideCursor={true}>
+            <h2 className="h2-title">
+                비 가시성 랩 코드<br />다양한 정보를 한눈에,
+            </h2>
+            <h3 className="h3-title">
+                새로운 데이터 구축 기술
+            </h3>
+        </Typing>
+    )
+};
 export default function Main() {
     const canvasRef = useRef<HTMLDivElement | null>(null);
     const Ball = {
         create: function (color, dx, dy) {
             const newBall = Object.create(this);
-            newBall.dx = dx;
+            newBall.velocity =
+                newBall.dx = dx;
             newBall.dy = dy;
+            newBall.radius = 500;
             newBall.width = 350;
             newBall.height = 350;
             newBall.element = document.createElement('div');
@@ -27,6 +42,7 @@ export default function Main() {
         },
         changeDirectionIfNecessary: function (x, y) {
             const ball = this as { [key: string]: any };
+
             const canvasWidth = (canvasRef.current as unknown as HTMLDivElement).clientWidth;
             const canvasHeight = (canvasRef.current as unknown as HTMLDivElement).clientHeight;
             if (x < 0 || x > canvasWidth - ball.width) {
@@ -36,30 +52,37 @@ export default function Main() {
                 ball.dy = -ball.dy;
             }
         },
+
         draw: function (x, y) {
+
             this.moveTo(x, y);
             const ball = this as { [key: string]: any };
             setTimeout(() => {
                 ball.changeDirectionIfNecessary(x, y);
                 ball.draw(x + ball.dx, y + ball.dy);
+                // let dx = another_ball.position.x - this.position.x, dy = another_ball.position.y - this.position.y
+                // let distance = Math.sqrt(dx*dx + dy*dy)
+                // if(distance < this.radius + another_ball.radius){
+                //   console.log("부딪힘")
+                //   let tmp = this.velocity
+                //   this.velocity = another_ball.velocity
+                //   another_ball.velocity = tmp
+                // }
             }, 1000 / 60);
         }
     };
     useEffect(() => {
-        const ballArr = [Ball.create("#F2A268", 4, 3), Ball.create("#73D27D", 2, 2), Ball.create("#EA43CF", 5, 8), Ball.create("#F2D568", 6, 5), Ball.create("#B3E052", 9, 2)]
-        ballArr.forEach((el, index) => {
-            el.draw(70, 0);
-        })
+        Ball.create("#F2A268", 4, 3).draw(70, 0);
+        Ball.create("#73D27D", 2, 2).draw(70, 0);
+        Ball.create("#EA43CF", 5, 8).draw(70, 0);
+        Ball.create("#F2D568", 6, 5).draw(70, 0);
+        Ball.create("#B3E052", 9, 2).draw(70, 0);
+
     }, [])
     return (
         <div className="bg" ref={canvasRef} >
             <div className="center-area">
-                <h2 className="h2-title">
-                    비 가시성 랩 코드<br />다양한 정보를 한눈에,
-                </h2>
-                <h3 className="h3-title">
-                    새로운 데이터 구축 기술
-                </h3>
+                <AnimatedTypingComponent />
                 <div className="btn-wrap">
                     <button onClick={() => window.open('https://apps.apple.com/kr/app/lab-code-scanner/id1597455005?app=itunes&ign-mpt=uo%3D4', '_blank')}
                         className="btn apple-download-btn"></button>
