75c3c81e48da8a7a74dcdd05f309744bd8151411
duatpwnd <duatpwnd1@naver.com>
2022-01-29 00:44:31 +0900

    refactoring in the team comp

--- src/pages/admin/team/CreateTeam.tsx
+++ src/pages/admin/team/CreateTeam.tsx
@@ -9,19 +9,29 @@ import _ from 'lodash'
 const CreateTeam = () => {
     const navigate = useNavigate();
     const [inputs, setInputs] = useState<{ [key: string]: any }>({});
+    const [phoneMessage, setPhoneMessage] = useState("");
+    const [emailMessage, setEmailMessage] = useState("");
+    const [businessNumberMessage, setBusinessNumberMessage] = useState("");
+    const [homePageMessage, setHomePageMessage] = useState("");
+    const [businessImage, setBusinessImage] = useState("");
     const create = () => {
         console.log(inputs);
         if (numberRegExp(inputs.businessNumber) == false) {
-            alert("올바른 번호의 형식이 아닙니다.")
-        } else if (homePageRegExp(inputs.homepage) == false) {
-            alert("올바른 주소의 형식이 아닙니다.")
-        } else if (phoneRegExp(inputs.managerPhone) == false) {
-            alert("올바른 번호의 형식이 아닙니다.")
-        } else if (emailRegExp(inputs.managerEmail) == false) {
-            alert("올바른 형식의 이메일 주소가 아닙니다.")
-        } else if (inputs.businessImage == null) {
-            alert("사업자 등록증을 첨부해주세요.")
-        } else {
+            setBusinessNumberMessage("올바른 번호의 형식이 아닙니다.")
+        }
+        if (homePageRegExp(inputs.homepage) == false) {
+            setHomePageMessage("올바른 번호의 형식이 아닙니다.")
+        }
+        if (phoneRegExp(inputs.managerPhone) == false) {
+            setPhoneMessage("올바른 번호의 형식이 아닙니다.")
+        }
+        if (emailRegExp(inputs.managerEmail) == false) {
+            setEmailMessage("올바른 형식의 이메일 주소가 아닙니다.")
+        }
+        if (inputs.businessImage == null) {
+            setBusinessImage("사업자 등록증을 첨부해주세요.")
+        }
+        if (numberRegExp(inputs.businessNumber) && homePageRegExp(inputs.homepage) && phoneRegExp(inputs.managerPhone) && emailRegExp(inputs.managerEmail) && inputs.businessImage != null) {
             console.log("생성요청", inputs);
             axios
                 .post(apiUrl.team, inputs)
@@ -36,8 +46,8 @@ const CreateTeam = () => {
     return (
         <main>
             <h2 className="h2-title">신청하기</h2>
-            <CompanyInfo useStateProperty={inputs} stateHandler={setInputs} />
-            <ManagerInfo useStateProperty={inputs} stateHandler={setInputs} children={<div className="row">
+            <CompanyInfo useStateProperty={inputs} stateHandler={setInputs} validationCheck={{ homePageMessage: homePageMessage, setHomePageMessage: setHomePageMessage, businessNumberMessage: businessNumberMessage, setBusinessNumberMessage: setBusinessNumberMessage, businessImage: businessImage, setBusinessImage: setBusinessImage }} />
+            <ManagerInfo useStateProperty={inputs} stateHandler={setInputs} validationCheck={{ setEmailMessage: setEmailMessage, emailMessage: emailMessage, phoneMessage: phoneMessage, setPhoneMessage: setPhoneMessage }} children={<div className="row">
                 <label htmlFor="managerPassword">비밀번호</label>
                 <input type="password" defaultValue={inputs.managerEmail} id="managerPassword" onChange={(e) => setInputs({ ...inputs, [e.target.id]: e.target.value })} />
             </div>} />
--- src/pages/admin/team/Team.tsx
+++ src/pages/admin/team/Team.tsx
@@ -9,6 +9,7 @@ import "./Team.scoped.scss"
 import { phoneRegExp, emailRegExp, numberRegExp, homePageRegExp } from 'src/utils/common';
 const debounce = _.debounce;
 const modify = (body) => {
+    console.log(body);
     const formData = new FormData();
     if (phoneRegExp(body.managerPhone) && emailRegExp(body.managerEmail) && numberRegExp(body.businessNumber) && homePageRegExp(body.homepage) && body.businessImage != null) {
         for (let key in body) {
@@ -25,14 +26,13 @@ const modify = (body) => {
 }
 export const ManagerInfo = (props) => {
     const { pathname } = useLocation();
-    const [phoneErrorMessage, setPhoneErrorMessage] = useState("");
-    const [emailErrorMessage, setEmailErrorMessage] = useState("");
+
     const phoneValueCheck = (e) => {
         if (phoneRegExp(e.target.value) == false) {
             props.stateHandler({ ...props.useStateProperty, [e.target.id]: e.target.value });
-            setPhoneErrorMessage("올바른 번호의 형식이 아닙니다.");
+            props.validationCheck.setPhoneMessage("올바른 번호의 형식이 아닙니다.");
         } else {
-            setPhoneErrorMessage("");
+            props.validationCheck.setPhoneMessage("");
             props.stateHandler({ ...props.useStateProperty, [e.target.id]: e.target.value });
             if (pathname == "/team") {
                 modify({ ...props.useStateProperty, [e.target.id]: e.target.value })
@@ -42,9 +42,9 @@ export const ManagerInfo = (props) => {
     const emailValueCheck = (e) => {
         if (emailRegExp(e.target.value) == false) {
             props.stateHandler({ ...props.useStateProperty, [e.target.id]: e.target.value });
-            setEmailErrorMessage("올바른 형식의 이메일 주소가 아닙니다.");
+            props.validationCheck.setEmailMessage("올바른 형식의 이메일 주소가 아닙니다.");
         } else {
-            setEmailErrorMessage("");
+            props.validationCheck.setEmailMessage("");
             props.stateHandler({ ...props.useStateProperty, [e.target.id]: e.target.value });
             if (pathname == "/team") {
                 modify({ ...props.useStateProperty, [e.target.id]: e.target.value })
@@ -67,28 +67,27 @@ export const ManagerInfo = (props) => {
             <div className="row">
                 <label htmlFor="managerPhone">연락처</label>
                 <input type="text" defaultValue={props.useStateProperty.managerPhone} id="managerPhone" onChange={debounce((e) => phoneValueCheck(e), 500)} />
-                <p className="warn-message">{phoneErrorMessage}</p>
+                <p className="warn-message">{props.validationCheck.phoneMessage}</p>
             </div>
             <div className="row">
                 <label htmlFor="managerEmail">이메일</label>
                 <input type="text" defaultValue={props.useStateProperty.managerEmail} id="managerEmail" onChange={debounce((e) => emailValueCheck(e), 500)} />
-                <p className="warn-message">{emailErrorMessage}</p>
+                <p className="warn-message">{props.validationCheck.emailMessage}</p>
             </div>
             {props.children}
         </section>
     )
 }
-export const CompanyInfo = ({ useStateProperty, stateHandler }) => {
-    const [businessNumberErrorMessage, setBusinessNumberErrorMessage] = useState("");
-    const [homePageErrorMessage, setHomepageErrorMessage] = useState("");
-    const [isActive, setActive] = useState(false)
+export const CompanyInfo = ({ useStateProperty, stateHandler, validationCheck }) => {
+    const [isActive, setActive] = useState(false);
+    const [link, fileLink] = useState("");
     const { pathname } = useLocation();
     const businessNumberValueCheck = (e) => {
         if (numberRegExp(e.target.value) == false) {
             stateHandler({ ...useStateProperty, [e.target.id]: e.target.value });
-            setBusinessNumberErrorMessage("올바른 번호의 형식이 아닙니다.");
+            validationCheck.setBusinessNumberMessage("올바른 번호의 형식이 아닙니다.");
         } else {
-            setBusinessNumberErrorMessage("");
+            validationCheck.setBusinessNumberMessage("");
             stateHandler({ ...useStateProperty, [e.target.id]: e.target.value });
             if (pathname == "/team") {
                 modify({ ...useStateProperty, [e.target.id]: e.target.value })
@@ -98,9 +97,9 @@ export const CompanyInfo = ({ useStateProperty, stateHandler }) => {
     const homePageValueCheck = (e) => {
         if (homePageRegExp(e.target.value) == false) {
             stateHandler({ ...useStateProperty, [e.target.id]: e.target.value });
-            setHomepageErrorMessage("올바른 주소의 형식이 아닙니다.");
+            validationCheck.setHomePageMessage("올바른 주소의 형식이 아닙니다.");
         } else {
-            setHomepageErrorMessage("");
+            validationCheck.setHomePageMessage("");
             stateHandler({ ...useStateProperty, [e.target.id]: e.target.value });
             if (pathname == "/team") {
                 modify({ ...useStateProperty, [e.target.id]: e.target.value })
@@ -108,6 +107,10 @@ export const CompanyInfo = ({ useStateProperty, stateHandler }) => {
         }
     }
     const notCheck = (e) => {
+        if (e.target.id == "businessImage") {
+            validationCheck.setBusinessImage("");
+            fileLink(URL.createObjectURL(e.target.files[0]))
+        }
         stateHandler({ ...useStateProperty, [e.target.id]: e.target.id == "businessImage" ? e.target.files[0] : e.target.value });
         if (pathname == "/team") {
             modify({ ...useStateProperty, [e.target.id]: e.target.id == "businessImage" ? e.target.files[0] : e.target.value })
@@ -117,7 +120,9 @@ export const CompanyInfo = ({ useStateProperty, stateHandler }) => {
         if (typeof useStateProperty.businessImage == "object") {
             return useStateProperty.businessImage.name
         } else {
-            return useStateProperty.businessImage
+            if (useStateProperty.businessImage != undefined) {
+                return useStateProperty.businessImage.replace(/^.*\//, '')
+            }
         }
     }, [useStateProperty.businessImage])
     return (
@@ -131,34 +136,39 @@ export const CompanyInfo = ({ useStateProperty, stateHandler }) => {
             <div className="row">
                 <label htmlFor="businessNumber">사업자등록번호</label>
                 <input type="text" defaultValue={useStateProperty.businessNumber} id="businessNumber" onChange={debounce((e) => businessNumberValueCheck(e), 500)} />
-                <p className="warn-message">{businessNumberErrorMessage}</p>
+                <p className="warn-message">{validationCheck.businessNumberMessage}</p>
             </div>
             <div className="row">
                 {
-                    isActive && <img src={useStateProperty.businessImage} onClick={() => setActive(false)} className="modal-image" />
+                    isActive && <img src={link || useStateProperty.businessImage} onClick={() => setActive(false)} className="modal-image" />
                 }
                 <label htmlFor="businessImage">사업자등록증</label>
                 <div className="business-image-area">
                     <div className="attach">
-                        <a href={useStateProperty.businessImage} onClick={() => setActive(true)} className="input-file" download="newfilename">{useStateProperty.businessImage == null ? "사업자등록증을 첨부해주세요." : getFileName}</a>
+                        <a href={link || useStateProperty.businessImage} onClick={() => useStateProperty.businessImage == null ? "" : setActive(true)} className="input-file" download="newfilename">{useStateProperty.businessImage == null ? "사업자등록증을 첨부해주세요." : getFileName}</a>
                         <input type="file" defaultValue={useStateProperty.businessImage} id="businessImage" onChange={(e) => notCheck(e)} />
                         <button className="delete-btn"></button>
                     </div>
                     <label htmlFor="businessImage" className="file">찾아보기</label>
                 </div>
-
+                <p className="warn-message">{validationCheck.businessImage}</p>
             </div>
 
             <div className="row">
                 <label htmlFor="homepage">홈페이지 주소</label>
                 <input type="text" defaultValue={useStateProperty.homepage} id="homepage" onChange={debounce((e) => homePageValueCheck(e), 500)} />
-                <p className="warn-message">{homePageErrorMessage}</p>
+                <p className="warn-message">{validationCheck.homePageMessage}</p>
             </div>
         </section>
     )
 }
 const Team = () => {
     const [inputs, setInputs] = useState({});
+    const [phoneMessage, setPhoneMessage] = useState("");
+    const [emailMessage, setEmailMessage] = useState("");
+    const [businessNumberMessage, setBusinessNumberMessage] = useState("");
+    const [homePageMessage, setHomePageMessage] = useState("");
+    const [businessImage, setBusinessImage] = useState("");
     const getTeamList = () => {
         axios
             .get(apiUrl.team)
@@ -176,8 +186,8 @@ const Team = () => {
         <main>
             <h2 className="h2-title">정보 입력</h2>
             <p className="message">정보를 변경하면 자동으로 저장됩니다.</p>
-            <CompanyInfo useStateProperty={inputs} stateHandler={setInputs} />
-            <ManagerInfo useStateProperty={inputs} stateHandler={setInputs} />
+            <CompanyInfo useStateProperty={inputs} stateHandler={setInputs} validationCheck={{ homePageMessage: homePageMessage, setHomePageMessage: setHomePageMessage, businessNumberMessage: businessNumberMessage, setBusinessNumberMessage: setBusinessNumberMessage, businessImage: businessImage, setBusinessImage: setBusinessImage }} />
+            <ManagerInfo useStateProperty={inputs} stateHandler={setInputs} validationCheck={{ setEmailMessage: setEmailMessage, emailMessage: emailMessage, phoneMessage: phoneMessage, setPhoneMessage: setPhoneMessage }} />
         </main >
     )
 }
