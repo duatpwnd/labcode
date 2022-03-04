80a9bea7ddd4b84f76296691b0ad93a25fa7fe20
duatpwnd <duatpwnd1@naver.com>
2022-03-04 19:04:27 +0900

    제품목록 일부 제품 엑박뜨는현상 해결

--- src/pages/admin/project/projects-classification/Classification.tsx
+++ src/pages/admin/project/projects-classification/Classification.tsx
@@ -84,7 +84,7 @@ const Classification = ({ eventHandler, inputs, isActive }: any) => {
             </div>
             {
                 countries.length != 0 && <div className="row">
-                    <label htmlFor="countryId">국가{String(inputs.countryId)}</label>
+                    <label htmlFor="countryId">국가</label>
                     <div className="select-box-wrap" style={isActive == false ? { pointerEvents: "none", background: "#ccc", borderRadius: "4px" } : { pointerEvents: "auto" }}>
                         <SelectBox
                             style={selectBoxStyle}
@@ -97,7 +97,7 @@ const Classification = ({ eventHandler, inputs, isActive }: any) => {
                 </div>
             }
             <div className="row">
-                <label htmlFor="industryId" >산업군{String(inputs.industryId)}</label>
+                <label htmlFor="industryId" >산업군</label>
                 <div className="select-box-wrap" style={isActive == false ? { pointerEvents: "none", background: "#ccc", borderRadius: "4px" } : { pointerEvents: "auto" }}>
                     <SelectBox
                         style={selectBoxStyle}
@@ -110,7 +110,7 @@ const Classification = ({ eventHandler, inputs, isActive }: any) => {
             </div>
             {
                 pathname.includes('defaultInfo') == false && <div className="row">
-                    <label htmlFor="teamId" className="team" >팀{String(inputs.teamId)}</label>
+                    <label htmlFor="teamId" className="team" >팀</label>
                     <div className="select-box-wrap" style={isActive == false ? { pointerEvents: "none", background: "#ccc", borderRadius: "4px" } : { pointerEvents: "auto" }}>
                         <SelectBox
                             style={selectBoxStyle}
@@ -125,7 +125,7 @@ const Classification = ({ eventHandler, inputs, isActive }: any) => {
             {
                 inputs.industryId != null && inputs.teamId != null &&
                 <div className="row">
-                    <label htmlFor="mainCategoryId" >대분류{String(inputs.mainCategoryId)}</label>
+                    <label htmlFor="mainCategoryId" >대분류</label>
                     <div className="select-box-wrap" style={isActive == false ? { pointerEvents: "none", background: "#ccc", borderRadius: "4px" } : { pointerEvents: "auto" }}>
                         <SelectBox
                             style={selectBoxStyle}
